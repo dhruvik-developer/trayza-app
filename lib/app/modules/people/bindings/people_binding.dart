da74b472be870616dbbc82e5507b7187f699b437
dhruvik-developer <barvaliyadhruvik27@gmail.com>
2026-04-20 17:25:14 +0530

    feat: add People module with binding and controller implementation

---
+++ lib/app/modules/people/bindings/people_binding.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import '../controllers/people_controller.dart';
+
+class PeopleBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<PeopleController>(() => PeopleController());
+  }
+}
